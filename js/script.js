23c2536bba80226f1c9285f746027c748b5e01c6
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-30 00:26:39 +0300

    prototype created from the Contact constructor method

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
 }
+// proto type create from a constructor
+Contact.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName;
+}
 // user interface
 $(document).ready(function () {
     $("form#new-contact").submit(function (e) {
@@ -10,8 +14,14 @@ $(document).ready(function () {
         var inputtedFirstName = $("input#new-first-name").val();
         var inputtedLastName = $("input#new-last-name").val();
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
+        $(".contact").last().click(function(){
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+        });
     })
 });
